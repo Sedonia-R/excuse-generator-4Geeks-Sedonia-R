e73f7d203c6847e219e26a2008f32f67004cd7f1
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-06-06 01:12:04 +0000

    tried unsuccessfully to manipulate the DOM and instead reverted to old work. lost updated changes when last codespace deleted

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ let action = ['ate', 'peed on', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
+function excuse(array) {
+  let randomArrayIndex = array[Math.floor(Math.random() * 4)];
+  return(randomArrayIndex);
+}
+
+document.write(excuse(who) + " " + excuse(action) + " " + excuse(what) + " " + excuse(when));
+
+// const excuseElement = document.createElement('p');
+// excuseElement.textContent = generateExcuse(who, action, what, when);
+// document.body.appendChild(excuseElement);
+
+// We "grab" the element by its id, there are many ways to "grab" html elements with js
+// const excuseElement = document.getElementById("excuse");
+// excuseElement.textContent = generateExcuse(who, action, what, when);
+// excuseElement.appendChild(hello)
+
+
 // function excuse(param1, param2, param3, param4) {
 //   let who = param1[Math.floor(Math.random() * 4)];
 //   let action = param2[Math.floor(Math.random() * 4)];
@@ -12,9 +29,4 @@ let when = ['before the class', 'when I was sleeping', 'while I was exercising',
 // }
 
 // document.write(excuse(who, action, what, when));
-
-function excuse(array) {
-  let randomArrayIndex = array[Math.floor(Math.random() * 4)];
-  return(randomArrayIndex);
-}
 
